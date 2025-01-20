3fdc54a6d535034627ab13bb4cff1a4cf1f4a428
oppenheimor <sjq15251852316@gmail.com>
2025-01-20 20:51:28 +0800

    chore add speed-insights

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
+import { SpeedInsights } from '@vercel/speed-insights/vue'
+
 useHead({
   title: 'Vitesse',
   meta: [
@@ -11,6 +13,7 @@ useHead({
 </script>
 
 <template>
+  <SpeedInsights />
   <nav-bar />
   <router-view />
 </template>
